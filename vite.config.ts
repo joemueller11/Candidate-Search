c8d44292dc5df0cba72f07b32fb4f2d2e47e9bc3
Joseph Mueller <joseph.m.email@gmail.com>
2025-03-20 20:50:20 -0400

    portaccess

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: '.env',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
+    strictPort: true,
+  }
 });
